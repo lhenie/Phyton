01e22e5eb01ca68f38a5500aff7e8e57f40f590f
lhenie <59660109+lhenie@users.noreply.github.com>
2021-05-16 13:53:02 +0800

    Create Adding_item_to_the_list.py

---
+++ Adding_item_to_the_list.py
@@ -0,0 +1,6 @@
+subjects =  ["Data Mining", "Software Engineering", "Networking", "Project Management", "Graphics Animation"]
+course = ["Information Technology", "Information Systems", "Computer Science"]
+
+subjects.append("Programming")
+
+print(subjects)
