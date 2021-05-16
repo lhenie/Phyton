ebebe46e8e2850739cb0f1e370c8d9aa3f17fdfb
lhenie <59660109+lhenie@users.noreply.github.com>
2021-05-16 14:15:49 +0800

    Create Arranging_all_item_in_order.py

---
+++ Arranging_all_item_in_order.py
@@ -0,0 +1,5 @@
+subjects = ["Data Mining", "Software Engineering", "Networking", "Project Management", "Graphics Animation"]
+course = ["Information Technology", "Information Systems", "Computer Science"]
+
+subjects.sort()
+print("Sorted list =",subjects)
