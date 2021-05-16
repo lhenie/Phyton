4f2eac41b9bb32ef7ea5e1160e1c0f095c5ebbb4
lhenie <59660109+lhenie@users.noreply.github.com>
2021-05-16 14:21:22 +0800

    Create Counting_all_item_in_the_list.py

---
+++ Counting_all_item_in_the_list.py
@@ -0,0 +1,6 @@
+subjects = ["Data Mining", "Software Engineering", "Networking", "Project Management", "Graphics Animation"]
+course = ["Information Technology", "Information Systems", "Computer Science"]
+
+number_of_elements = len(subjects)
+print(subjects)
+print("The number of subjects in the list: ", number_of_elements)
