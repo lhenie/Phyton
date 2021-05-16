76bf0ca8d3826c8954369d0386c6c534027487f4
lhenie <59660109+lhenie@users.noreply.github.com>
2021-05-16 14:04:59 +0800

    Create Taking_item_from_the_list.py

---
+++ Taking_item_from_the_list.py
@@ -0,0 +1,4 @@
+subjects = ["Data Mining", "Software Engineering", "Networking", "Project Management", "Graphics Animation"]
+course = ["Information Technology", "Information Systems", "Computer Science"]
+
+print(course[1])
